9ed6ef89fbc51ba5c85d43d823fc402a185ee451
maksym.vakoniuk <makswk04@gmail.com>
2023-03-18 19:30:09 +0200

    Initial commit

---
+++ app/src/main/java/com/maxxxwk/testtask/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.maxxxwk.testtask
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.maxxxwk.testtask.ui.theme.TestTaskTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestTaskTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {}
+            }
+        }
+    }
+}
