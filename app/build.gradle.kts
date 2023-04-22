a5a530e3168d6924d23bb9582b6f2de916ccf0e2
maksym.vakoniuk <makswk04@gmail.com>
2023-04-22 12:59:28 +0300

    migrated from Groovy to Kotlin DSL in gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
+}
+
+android {
+    namespace = "com.maxxxwk.testtask"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.maxxxwk.testtask"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables.useSupportLibrary = true
+
+        buildConfigField("String", "BASE_URL", "\"https://cr-test-ribu2uaqea-ey.a.run.app/\"")
+        buildConfigField("String", "APP_NAME", "\"cuton\"")
+        buildConfigField("int", "BUILD_NUMBER", "36")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions.jvmTarget = "1.8"
+    buildFeatures.compose = true
+    composeOptions.kotlinCompilerExtensionVersion = "1.4.6"
+    packagingOptions.resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+}
+
+dependencies {
+    implementation(Libraries.LIFECYCLE_RUNTIME)
+    implementation(Libraries.LIFECYCLE_VIEWMODEL_COMPOSE)
+
+    implementation(platform(BillsOfMaterials.COMPOSE))
+    implementation(Libraries.COMPOSE_UI)
+    implementation(Libraries.COMPOSE_MATERIAL)
+    implementation(Libraries.COMPOSE_UI_TOOLING_PREVIEW)
+    debugImplementation(DebugLibraries.COMPOSE_UI_TOOLING)
+    implementation(Libraries.COMPOSE_NAVIGATION)
+    implementation(Libraries.ACTIVITY_COMPOSE)
+
+    implementation(Libraries.COIL)
+
+    implementation(Libraries.DAGGER)
+    kapt(KaptCompilers.DAGGER)
+
+    implementation(Libraries.KOTLINX_COLLECTIONS_IMMUTABLE)
+    implementation(Libraries.KOTLINX_SERIALIZATION_JSON)
+
+    implementation(platform(BillsOfMaterials.OKHTTP))
+    implementation(Libraries.OKHTTP)
+    implementation(Libraries.OKHTTP_LOGGING_INTERCEPTOR)
+
+    implementation(Libraries.RETROFIT)
+    implementation(Libraries.RETROFIT_KOTLINX_SERIALIZATION_CONVERTER)
+
+
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,41 @@
+object Versions {
+    const val LIFECYCLE = "2.6.1"
+    const val DAGGER = "2.45"
+}
+
+object BillsOfMaterials {
+    const val COMPOSE = "androidx.compose:compose-bom:2023.04.01"
+    const val OKHTTP = "com.squareup.okhttp3:okhttp-bom:4.10.0"
+}
+
+object Libraries {
+    const val LIFECYCLE_RUNTIME = "androidx.lifecycle:lifecycle-runtime:${Versions.LIFECYCLE}"
+    const val LIFECYCLE_VIEWMODEL_COMPOSE = "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.LIFECYCLE}"
+
+    const val COMPOSE_UI = "androidx.compose.ui:ui"
+    const val COMPOSE_UI_TOOLING_PREVIEW = "androidx.compose.ui:ui-tooling-preview"
+    const val COMPOSE_MATERIAL = "androidx.compose.material:material"
+    const val COMPOSE_NAVIGATION = "androidx.navigation:navigation-compose:2.5.3"
+    const val ACTIVITY_COMPOSE = "androidx.activity:activity-compose:1.7.1"
+
+    const val COIL = "io.coil-kt:coil-compose:2.3.0"
+
+    const val DAGGER = "com.google.dagger:dagger:${Versions.DAGGER}"
+
+    const val KOTLINX_COLLECTIONS_IMMUTABLE = "org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5"
+    const val KOTLINX_SERIALIZATION_JSON = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0"
+
+    const val OKHTTP = "com.squareup.okhttp3:okhttp"
+    const val OKHTTP_LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor"
+
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:2.9.0"
+    const val RETROFIT_KOTLINX_SERIALIZATION_CONVERTER = "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0"
+}
+
+object DebugLibraries {
+    const val COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling"
+}
+
+object KaptCompilers {
+    const val DAGGER = "com.google.dagger:dagger-compiler:${Versions.DAGGER}"
+}
